7d807758c6ffaa3d4ef97be13a48e2e83bfa111f
Feridun Halil ERDEN <122286614+hllerden@users.noreply.github.com>
2024-03-22 12:20:13 +0300

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -302,60 +302,7 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-/*
-void usart_rx_check(void)
-{
-    static size_t old_pos;
 
-	size_t pos;
-	size_t dmaWhatPose=LL_DMA_GetDataLength(DMA1, LL_DMA_CHANNEL_5);
-	 pos = ARRAY_LEN(usart_rx_dma_buffer) - LL_DMA_GetDataLength(DMA1, LL_DMA_CHANNEL_5);
-	 if (pos != old_pos) {                       // Check change in received data //
-	         if (pos > old_pos) {                    // Current position is over previous one //
-	             //
-	              // Processing is done in "linear" mode.
-	              //
-	              // Application processing is fast with single data block,
-	              // length is simply calculated by subtracting pointers
-	              //
-	              // [   0   ]
-	              // [   1   ] <- old_pos |------------------------------------|
-	              // [   2   ]            |                                    |
-	              // [   3   ]            | Single block (len = pos - old_pos) |
-	              // [   4   ]            |                                    |
-	              // [   5   ]            |------------------------------------|
-	              // [   6   ] <- pos
-	              // [   7   ]
-	              // [ N - 1 ]
-	              ///
-	             usart_process_data(&usart_rx_dma_buffer[old_pos], pos - old_pos,false,-1);
-	         } else {
-	             ///
-	              // Processing is done in "overflow" mode..
-	              //
-	              // Application must process data twice,
-	              // since there are 2 linear memory blocks to handle
-	              //
-	              // [   0   ]            |---------------------------------|
-	              // [   1   ]            | Second block (len = pos)        |
-	              // [   2   ]            |---------------------------------|
-	              // [   3   ] <- pos
-	              // [   4   ] <- old_pos |---------------------------------|
-	              // [   5   ]            |                                 |
-	              // [   6   ]            | First block (len = N - old_pos) |
-	              // [   7   ]            |                                 |
-	              // [ N - 1 ]            |---------------------------------|
-	              ///
-	             usart_process_data(&usart_rx_dma_buffer[old_pos], ARRAY_LEN(usart_rx_dma_buffer) - old_pos,false,-1);
-	             if (pos > 0) {
-	                 usart_process_data(&usart_rx_dma_buffer[0], pos,false,-1);
-	             }
-	         }
-	         old_pos = pos;                          /// Save current position as old for next transfers ///
-	     }
-}
-
-*/
 void usart_rx_check(void)
 {
     static size_t old_pos;
